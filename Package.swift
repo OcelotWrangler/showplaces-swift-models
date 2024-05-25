9b7a75c4839cc9e9bc6d280b1d02687841a9f101
Kevin Barnes <kevin@donniebarnes.com>
2024-05-25 12:14:07 -0400

    Rename to ShowplacesModels

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ShowplaceModels",
+    name: "ShowplacesModels",
     platforms: [
         .macOS(.v13),
         .iOS(.v15)
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ShowplaceModels",
-            targets: ["ShowplaceModels"]
+            name: "ShowplacesModels",
+            targets: ["ShowplacesModels"]
         ),
     ], 
     dependencies: [
@@ -23,14 +23,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ShowplaceModels",
+            name: "ShowplacesModels",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
             ]
         ),
         .testTarget(
-            name: "ShowplaceModelsTests",
-            dependencies: ["ShowplaceModels"]
+            name: "ShowplacesModelsTests",
+            dependencies: ["ShowplacesModels"]
         ),
     ]
 )
