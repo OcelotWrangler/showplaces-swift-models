cfbb1ec4ffa77089460ec93d1e82c8946d9f33c4
Kevin Barnes <kevin@donniebarnes.com>
2024-08-08 19:25:47 -0400

    User dto changes

--- Sources/ShowplacesModels/DTOs/Update/UpdateUserDTO.swift
+++ Sources/ShowplacesModels/DTOs/Update/UpdateUserDTO.swift
@@ -1,28 +1,28 @@
 //
-//  CreateUserDTO.swift
-//  
+//  UpdateUserDTO.swift
 //
-//  Created by Kevin Barnes on 5/23/24.
+//
+//  Created by Kevin Barnes on 8/5/24.
 //
 
 import Vapor
 
-public struct CreateUserDTO: Content {
+public struct UpdateUserDTO: Content {
     
     public var email: String
     public var firstName: String?
     public var lastName: String?
-    public var password: String
+    public var profilePictureKey: String?
     
     public init(
         email: String,
         firstName: String? = nil,
         lastName: String? = nil,
-        password: String
+        profilePictureKey: String? = nil
     ) {
         self.email = email
         self.firstName = firstName
         self.lastName = lastName
-        self.password = password
+        self.profilePictureKey = profilePictureKey
     }
 }
