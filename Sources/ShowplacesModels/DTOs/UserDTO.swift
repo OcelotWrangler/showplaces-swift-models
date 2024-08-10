cc14a2b67130d9d72b26f4f5eade01399119128c
Kevin Barnes <kevin@donniebarnes.com>
2024-08-09 21:23:44 -0400

    Add new stuff to user dto

--- Sources/ShowplacesModels/DTOs/UserDTO.swift
+++ Sources/ShowplacesModels/DTOs/UserDTO.swift
@@ -12,6 +12,9 @@ public struct UserDTO: Content, Hashable, Identifiable {
     public var id: UUID
     public var email: String
     public var username: String
+    public var firstName: String?
+    public var lastName: String?
+    public var profilePictureKey: String?
     public var created: Date
     public var updated: Date
     
@@ -19,12 +22,18 @@ public struct UserDTO: Content, Hashable, Identifiable {
         id: UUID,
         email: String,
         username: String,
+        firstName: String? = nil,
+        lastName: String? = nil,
+        profilePictureKey: String? = nil,
         created: Date,
         updated: Date
     ) {
         self.id = id
         self.email = email
         self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.profilePictureKey = profilePictureKey
         self.created = created
         self.updated = updated
     }
