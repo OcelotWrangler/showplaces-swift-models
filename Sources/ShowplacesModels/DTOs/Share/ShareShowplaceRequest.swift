311853446bf4bfb4eee5bb8bbbcbc0ab6f1c96da
Kevin Barnes <kevin@donniebarnes.com>
2024-11-01 12:08:24 -0400

    One share showplace dto and remove group stuff for now

--- Sources/ShowplacesModels/DTOs/Share/ShareShowplaceRequest.swift
+++ Sources/ShowplacesModels/DTOs/Share/ShareShowplaceRequest.swift
@@ -1,5 +1,5 @@
 //
-//  ShareLiveGroupRequest.swift
+//  ShareShowplaceRequest.swift
 //  ShowplacesModels
 //
 //  Created by Kevin Barnes on 10/31/24.
@@ -7,20 +7,23 @@
 
 import Vapor
 
-public struct ShareLiveGroupRequest: Content {
+public struct ShareShowplaceRequest: Content {
     
-    public var groupId: UUID
+    public var showplaceId: UUID
+    public var shareType: ShareType
     public var accessLevel: AccessLevel
     public var maxUses: Int?
     public var expiration: Date?
     
     public init(
-        groupId: UUID,
+        showplaceId: UUID,
+        shareType: ShareType,
         accessLevel: AccessLevel,
         maxUses: Int? = nil,
         expiration: Date? = nil
     ) {
-        self.groupId = groupId
+        self.showplaceId = showplaceId
+        self.shareType = shareType
         self.accessLevel = accessLevel
         self.maxUses = maxUses
         self.expiration = expiration
