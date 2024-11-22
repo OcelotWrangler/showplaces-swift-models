21bebf266448abb76fadb667da7b8c9b81a887b3
Kevin Barnes <kevin@donniebarnes.com>
2024-11-22 10:03:53 -0500

    Add description to UpdateTagDTO

--- Sources/ShowplacesModels/DTOs/Update/UpdateTagDTO.swift
+++ Sources/ShowplacesModels/DTOs/Update/UpdateTagDTO.swift
@@ -11,6 +11,7 @@ public struct UpdateTagDTO: Content, Hashable, Identifiable {
     
     public let id: UUID
     public var title: String
+    public var description: String?
     public var color: String?
     public var sfSymbol: String?
     public var archived: Bool
@@ -18,12 +19,14 @@ public struct UpdateTagDTO: Content, Hashable, Identifiable {
     public init(
         id: UUID,
         title: String,
+        description: String? = nil,
         color: String? = nil,
         sfSymbol: String? = nil,
         archived: Bool
     ) {
         self.id = id
         self.title = title
+        self.description = description
         self.color = color
         self.sfSymbol = sfSymbol
         self.archived = archived
