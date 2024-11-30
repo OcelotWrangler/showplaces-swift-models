81228de87df0d2f7069cca767e625e8b66389609
Kevin Barnes <kevin@donniebarnes.com>
2024-11-30 11:41:48 -0500

    Add device id to sign in/up with Apple requests

--- Sources/ShowplacesModels/DTOs/Auth/SignInWithAppleRequest.swift
+++ Sources/ShowplacesModels/DTOs/Auth/SignInWithAppleRequest.swift
@@ -10,8 +10,13 @@ import Vapor
 public struct SignInWithAppleRequest: Content {
     
     public var identityToken: String
+    public var deviceId: UUID
     
-    public init(identityToken: String) {
+    public init(
+        identityToken: String,
+        deviceId: UUID
+    ) {
         self.identityToken = identityToken
+        self.deviceId = deviceId
     }
 }
--- Sources/ShowplacesModels/DTOs/Auth/SignUpWithAppleRequest.swift
+++ Sources/ShowplacesModels/DTOs/Auth/SignUpWithAppleRequest.swift
@@ -13,16 +13,19 @@ public struct SignUpWithAppleRequest: Content {
     public var email: String
     public var firstName: String?
     public var lastName: String?
+    public var deviceId: UUID
     
     public init(
         identityToken: String,
         email: String,
         firstName: String? = nil,
-        lastName: String? = nil
+        lastName: String? = nil,
+        deviceId: UUID
     ) {
         self.identityToken = identityToken
         self.email = email
         self.firstName = firstName
         self.lastName = lastName
+        self.deviceId = deviceId
     }
 }
